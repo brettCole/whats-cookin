c59978833fe5de1efa5b99db0bdae78e3eef5f62
github username <github email address>
2017-02-02 08:48:24 -0500

    added spec file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :ingredients
   root 'pages#main'
 
+  resources :ingredients
   resources :users
   resources :recipes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,8 @@
 ActiveRecord::Schema.define(version: 20170202041907) do
 
   create_table "ingredients", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "recipe_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
